f10a16ab7b6d6a57977a09c7c3a18d80214bfabf
lydiaaawren <165889010+lydiaaawren@users.noreply.github.com>
2025-07-10 15:13:10 +0100

    added file unpacking

--- amp_ex.py
+++ amp_ex.py
@@ -3,7 +3,11 @@
 
 # Load libraries
 import os
-import requests
+import requests       
+import zipfile     
+import gzip        
+import shutil     
+import tempfile    
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 
@@ -42,5 +46,31 @@ if response.status_code == 200:
 else:
     # Need to return error
     print(f'Error {response.status_code}: {response.text}')
+
+
+# Create a temporary directory for extraction
+temp_dir = tempfile.mkdtemp()
+
+# Create local output directory
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
+
+# Unpack zip
+with zipfile.ZipFile("data.zip", "r") as zip_ref:
+    zip_ref.extractall(temp_dir)
+
+# Locate the folders -> need file path
+day_folder = next(f for f in os.listdir(temp_dir) if f.isdigit())
+day_path = os.path.join(temp_dir, day_folder)
+
+# triple unpack jsons
+for root, _, files in os.walk(day_path):
+    for file in files:
+        if file.endswith('.gz'):
+            gz_path = os.path.join(root, file)
+            json_filename = file[:-3]
+            output_path = os.path.join(data_dir, json_filename)
+            with gzip.open(gz_path, 'rb') as gz_file, open(output_path, 'wb') as out_file:
+                shutil.copyfileobj(gz_file, out_file)
 
 
